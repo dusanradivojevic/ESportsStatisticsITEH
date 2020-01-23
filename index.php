844a0dd0a1c940df3cbe4aebe21d7e684afaab41
dusanradivojevic <r.dusan97@gmail.com>
2020-01-23 11:16:21 +0100

    Unnecessary files are deleted.

--- index.php
+++ index.php
@@ -22,7 +22,6 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="/jquery-3.4.1.min.js"></script>
     <link rel="stylesheet" type="text/css" href="DataTables-1.10.4/media/css/jquery.dataTables.min.css" />
     <script src="DataTables-1.10.4/media/js/jquery.dataTables.min.js"></script>
 
